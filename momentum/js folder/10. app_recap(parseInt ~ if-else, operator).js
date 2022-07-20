815c6ef1ae8f37806bec808ecea790843d328240
Koong_i's github <mybac1234@naver.com>
2022-07-20 14:10:00 +0900

    js lecture upload
    - if - else lecture upload
    - mindmap upload

---
+++ momentum/js folder/10. app_recap(parseInt ~ if-else, operator).js
@@ -0,0 +1,22 @@
+const age = parseInt(prompt('How old are you?'));
+
+'lalala'
+
+if (isNaN(age) || age < 0) {
+    console.log('Please write a real positive number');
+} else if (age < 18) {
+    console.log('You are too young');
+} else if (age >= 18 && age <= 50) {
+                    // && : and operator , 두가지 조건을 모두 충족 
+                    // || : or operator
+    console.log('You can drink');
+} else if(age > 50 && age <= 80) {
+    console.log('You should exercise');
+    // console.log('You can\'t drink');
+} else if (age === 100) {           // === : 조건이 같은지를 확인, !== : 조건이 다른지 확인
+    console.log('Wow you are wise');
+} else if (age > 80) {
+    console.log('You can do whatever you want');
+}
+
+
